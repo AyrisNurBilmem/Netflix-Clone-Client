8661ea015f21afbe2507a7ff9c16df18c3f85b8e
chottomatta <bilmem1499@gmail.com>
2022-02-06 23:12:04 +0200

    Finished Sign Up Card

--- src/pages/signin/Signin.js
+++ src/pages/signin/Signin.js
@@ -4,9 +4,11 @@ import validate from "./validateInfo";
 import { Link } from "react-router-dom";
 import styles from "./signin.module.scss";
 import netflix from "../../images/logo.png";
+import Footer from "../../components/footer/Footer";
 
 const Signin = () => {
   const { handleChange, values, handleSubmit, errors } = useForm(validate);
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className={styles.generalDiv}>
@@ -28,6 +30,7 @@ const Signin = () => {
                       autoComplete="off"
                       onChange={handleChange}
                       value={values.email}
+                      required
                     />
                     <label className={styles.inputLabel}>
                       <span className={styles.labelContent}>
@@ -47,6 +50,7 @@ const Signin = () => {
                       autoComplete="off"
                       onChange={handleChange}
                       value={values.password}
+                      required
                     />
                     <label className={styles.inputLabel}>
                       <span className={styles.labelContent}>Password</span>
@@ -61,7 +65,7 @@ const Signin = () => {
                   <div className={styles.formHelp}>
                     <div className={styles.rememberMe}>
                       <input className={styles.checkbox} type="checkbox" />
-                      <label className={styles.remeberMeLabel}>
+                      <label className={styles.rememberMeLabel}>
                         Remember Me
                       </label>
                     </div>
@@ -71,10 +75,81 @@ const Signin = () => {
                   </div>
                 </form>
               </div>
-              <div className={styles.signinOther}></div>
+            </div>
+            <div className={styles.signinOther}>
+              <form
+                className={styles.facebookForm}
+                onSubmit={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <div className={styles.facebookDiv}>
+                  <button className={styles.facebookButton}>
+                    <div className={styles.fbLogin}>
+                      <img
+                        className={styles.fbImg}
+                        src="https://assets.nflxext.com/ffe/siteui/login/images/FB-f-Logo__blue_57.png"
+                        alt="fb"
+                      />
+                      <span className={styles.facebookButtonText}>
+                        Login with Facebook
+                      </span>
+                    </div>
+                  </button>
+                </div>
+                <div className={styles.signupNow}>
+                  <p className={styles.signupPara}>
+                    New to Netflix?
+                    <Link to="/" className={styles.signupLink}>
+                      Sign Up Now
+                    </Link>
+                    .
+                  </p>
+                </div>
+                <div className={styles.termsOfUse}>
+                  <p className={styles.termsOfUsePara}>
+                    <span className={styles.termsOfUseText}>
+                      This page is protected by Google reCAPTCHA to ensure
+                      you're not a bot.
+                    </span>
+                    <button
+                      onClick={() => setIsOpen(!isOpen)}
+                      className={styles.termsOfUseButton}
+                      type="submit"
+                    >
+                      Learn more.
+                    </button>
+                  </p>
+                  {isOpen === true && (
+                    <div className={styles.learnMore}>
+                      <span>
+                        The information collected by Google reCAPTCHA is subject
+                        to the Google
+                        <a
+                          className={styles.termsOfUseLink}
+                          href="https://policies.google.com/privacy"
+                        >
+                          Privacy Policy
+                        </a>
+                        and
+                        <a
+                          className={styles.termsOfUseLink}
+                          href="https://policies.google.com/terms"
+                        >
+                          Terms of Service
+                        </a>
+                        , and is used for providing, maintaining, and improving
+                        the reCAPTCHA service and for general security purposes
+                        (it is not used for personalized advertising by Google).
+                      </span>
+                    </div>
+                  )}
+                </div>
+              </form>
             </div>
           </div>
         </div>
+        <Footer />
       </div>
     </>
   );
