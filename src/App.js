fcb1d9d17028e02c75e7af94972112c4b4a975de
chottomatta <bilmem1499@gmail.com>
2022-02-07 22:43:42 +0200

    Added new parts for signin

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Landing from "./pages/landing/Landing";
 import { Route, Routes } from "react-router-dom";
 import Signin from "./pages/signin/Signin";
+import FirstStep from "./pages/registration/FirstStep";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/signin/registration" element={<FirstStep />} />
       </Routes>
     </div>
   );
